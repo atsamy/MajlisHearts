639e7dc76fd13bc9a49b408889afe96abaf674da
Ahmed <amtsamy@gmail.com>
2022-04-28 18:55:28 +0200

    game UI

---
+++ Assets/PlayerDetails.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDetails : MonoBehaviour
+{
+    [SerializeField]
+    Image avatarImage;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text nameText;
+    [SerializeField]
+    Text levelText;
+    [SerializeField]
+    GameObject[] DoubleCards;
+    public void SetPlayer(string avatar,string name,string level)
+    {
+        avatarImage.sprite = Resources.Load<Sprite>("Avatar/Face/" + avatar);
+        nameText.text = name;
+        levelText.text = level;
+        scoreText.text = "0";
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void ShowDouble(int index)
+    {
+        DoubleCards[index].SetActive(true);
+    }
+
+    public void HideDouble(int index)
+    {
+        DoubleCards[index].SetActive(false);
+    }
+}
