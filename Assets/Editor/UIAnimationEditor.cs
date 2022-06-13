630a0a22bfbf8d12c52c735299b0bc114f4bb096
Ahmed <amtsamy@gmail.com>
2022-06-13 17:10:50 +0200

    animation script

--- Assets/Editor/UIAnimationEditor.cs
+++ Assets/Editor/UIAnimationEditor.cs
@@ -11,27 +11,92 @@ public class UIAnimationEditor : Editor
     {
         base.OnInspectorGUI();
 
-        //SideMenu mySideMenu = target as SideMenu;
         UIAnimation animation = ((UIAnimation)target);
-        //MenusManager mM = FindObjectOfType<MenusManager>();
 
-        if (GUILayout.Button("Add Move"))
+        EditorGUILayout.LabelField("Move");
+        ShowTransformAnimation("Move", animation.MoveAnimations);
+        AddMoveButtons(animation.MoveAnimations);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Scale");
+        ShowTransformAnimation("Scale", animation.ScaleAnimation);
+        AddScaleButtons(animation.ScaleAnimation);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Color");
+        ShowColorAnimatio("Color", animation, animation.ColorAnimation);
+    }
+
+    void ShowTransformAnimation(string animationName, List<TransformAnimationBase> moveList)
+    {
+        EditorGUILayout.BeginVertical();
+
+        if (moveList == null)
+            moveList = new List<TransformAnimationBase>();
+
+        for (int i = 0; i < moveList.Count; i++)
         {
-            animation.AddMove();
-        }
+            EditorGUILayout.LabelField(animationName + " Animation:" + i);
 
-        foreach (var item in animation.MoveAnimations)
-        {
-            foreach (var moveItem in animation.MoveAnimations)
-            {
-                EditorGUILayout.LabelField("Move Animation:1");
-
-                moveItem.Target = EditorGUILayout.Vector3Field("Target", moveItem.Target);
-                moveItem.Duration = EditorGUILayout.FloatField("Duration", moveItem.Duration);
-                //moveItem.Duration = EditorGUILayout.FloatField("Duration", moveItem.Duration);
-                //moveItem.Reverse = EditorGUILayout.fi("Duration", moveItem.Duration);
-            }
+            moveList[i].Start = EditorGUILayout.Vector3Field("Start", moveList[i].Start);
+            moveList[i].Duration = EditorGUILayout.FloatField("Duration", moveList[i].Duration);
+            moveList[i].StartAfter = EditorGUILayout.FloatField("Start After", moveList[i].StartAfter);
+            moveList[i].Reverse = EditorGUILayout.Toggle("Reverse", moveList[i].Reverse);
         }
     }
 
+    void AddMoveButtons(List<TransformAnimationBase> moveList)
+    {
+        if (GUILayout.Button("Add Move"))
+        {
+            moveList.Add(new MoveAnimation());
+        }
+        if (GUILayout.Button("Remove Move"))
+        {
+            moveList.RemoveAt(moveList.Count - 1);
+        }
+    }
+
+    void AddScaleButtons(List<TransformAnimationBase> moveList)
+    {
+        if (GUILayout.Button("Add Scale"))
+        {
+            moveList.Add(new ScaleAnimation());
+        }
+        if (GUILayout.Button("Remove Scale"))
+        {
+            moveList.RemoveAt(moveList.Count - 1);
+        }
+    }
+
+    void ShowColorAnimatio(string animationName, UIAnimation animation, List<ColorAnimation> colorList)
+    {
+        //EditorGUILayout.BeginVertical();
+
+        if (colorList == null)
+            colorList = new List<ColorAnimation>();
+
+        for (int i = 0; i < colorList.Count; i++)
+        {
+            EditorGUILayout.LabelField(animationName + " Animation:" + i);
+
+            colorList[i].Target = EditorGUILayout.ColorField("Target", colorList[i].Target);
+            colorList[i].Duration = EditorGUILayout.FloatField("Duration", colorList[i].Duration);
+            colorList[i].StartAfter = EditorGUILayout.FloatField("Start After", colorList[i].StartAfter);
+            colorList[i].Reverse = EditorGUILayout.Toggle("Reverse", colorList[i].Reverse);
+        }
+
+        if (GUILayout.Button("Add " + animationName))
+        {
+            animation.ColorAnimation.Add(new ColorAnimation());
+        }
+        if (GUILayout.Button("Remove " + animationName))
+        {
+            animation.ColorAnimation.RemoveAt(animation.ColorAnimation.Count - 1);
+        }
+
+        //EditorGUILayout.EndVertical();
+    }
 }
--- Assets/UIAnimation.cs
+++ Assets/UIAnimation.cs
@@ -5,17 +5,52 @@ using DG.Tweening;
 using UnityEngine.UI;
 using System;
 using UnityEditor;
+using System.Threading.Tasks;
 
 public class UIAnimation : MonoBehaviour
 {
     [HideInInspector]
-    public List<MoveAnimation> MoveAnimations;
+    public List<TransformAnimationBase> MoveAnimations;
+    [HideInInspector]
+    public List<TransformAnimationBase> ScaleAnimation;
     [HideInInspector]
     public List<ColorAnimation> ColorAnimation;
 
-    public void AddMove()
+    public bool PlayOnEnable;
+    Image image;
+
+    private void Awake()
     {
-        MoveAnimations.Add(new MoveAnimation(transform));
+        image = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        if (PlayOnEnable)
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        foreach (var item in MoveAnimations)
+        {
+            item.Set(transform);
+            item.Play();
+        }
+
+        foreach (var item in ScaleAnimation)
+        {
+            item.Set(transform);
+            item.Play();
+        }
+
+        foreach (var item in ColorAnimation)
+        {
+            item.Set(image);
+            item.Play();
+        }
     }
 }
 
@@ -24,38 +59,83 @@ public class AnimationBase
 {
     public float Duration;
     public bool Reverse;
+    public float StartAfter;
 
-    public virtual void Play() { }
+    public async void Play() 
+    {
+        await Task.Delay((int)(StartAfter * 100));
+
+        if (Reverse)
+            PlayReverse();
+        else
+            PlaySingle();
+    }
+    protected virtual void PlaySingle() { }
+    protected virtual void PlayReverse() { }
 }
 [System.Serializable]
-public class MoveAnimation : AnimationBase
+public class TransformAnimationBase:AnimationBase
 {
-    public Vector3 Target;
-    Transform transform;
-
-    public MoveAnimation(Transform transform)
+    public void Set(Transform transform)
     {
         this.transform = transform;
     }
 
-    public override void Play()
+    public Vector3 Start;
+    protected Transform transform;
+}
+
+[System.Serializable]
+public class MoveAnimation : TransformAnimationBase
+{
+    protected override void PlaySingle()
     {
+        Vector3 Target = transform.position;
+        transform.position = Start;
         transform.DOMove(Target, Duration);
     }
+
+    protected override void PlayReverse()
+    {
+        Vector3 Target = transform.position;
+        transform.position = Start;
+        transform.DOMove(Target, Duration).SetLoops(1, LoopType.Yoyo);
+    }
 }
+
 [System.Serializable]
 public class ColorAnimation : AnimationBase
 {
     public Color Target;
     Image image;
 
-    public ColorAnimation(Image image)
+    public void Set(Image image)
     {
         this.image = image;
     }
-
-    public override void Play()
+    protected override void PlaySingle()
     {
         image.DOColor(Target, Duration);
+    }
+    protected override void PlayReverse()
+    {
+        image.DOColor(Target, Duration).SetLoops(1, LoopType.Yoyo);
+    }
+}
+
+public class ScaleAnimation : TransformAnimationBase
+{
+    protected override void PlaySingle()
+    {
+        Vector3 Target = transform.localScale;
+        transform.localScale = Start;
+        transform.DOScale(Target, Duration);
+    }
+
+    protected override void PlayReverse()
+    {
+        Vector3 Target = transform.localScale;
+        transform.localScale = Start;
+        transform.DOScale(Target, Duration).SetLoops(1, LoopType.Yoyo);
     }
 }
