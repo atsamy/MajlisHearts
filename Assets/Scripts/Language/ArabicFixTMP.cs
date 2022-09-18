ae2505ca55f531bda0ad8a0012e1bccdcaac760f
atsamy <amtsamy@gmail.com>
2022-09-18 22:08:36 +0800

    arabic fix

---
+++ Assets/Scripts/Language/ArabicFixTMP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ArabicSupport;
+using TMPro;
+
+public class ArabicFixTMP : MonoBehaviour 
+{
+	void Awake () 
+	{
+		TextMeshProUGUI text;
+		if ((text = GetComponent<TextMeshProUGUI>()) != null)
+		{
+			text.text = ArabicFixer.Fix(text.text);
+		}
+	}
+}
