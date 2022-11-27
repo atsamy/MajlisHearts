8cfdb41bc57ae93226172ec7032cb42c1e98e12a
atsamy <amtsamy@gmail.com>
2022-11-27 18:36:39 +0700

    effect

--- Assets/Scripts/TilableEditableItem.cs
+++ Assets/Scripts/TilableEditableItem.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TilableEditableItem : SingleEditableItem
 {
     SpriteRenderer[] allTiles;
-
+    [SerializeField]
+    Material floorMaterial;
     public override void ChangeItem(int index)
     {
         //SetModified(index);
@@ -42,6 +44,10 @@ public class TilableEditableItem : SingleEditableItem
         selectedIndex = index;
         modified = true;
         modifiedSprite = allTiles[0].sprite;
+
+        if (floorMaterial != null)
+            StartCoroutine(ShowEffect());
+            //floorMaterial?.DOFloat(0.5f, "_Intensity",0.25f).SetDelay(1).SetLoops(2,LoopType.Yoyo);//.SetFloat("_Intensity",).do
     }
 
     public override void Init()
@@ -51,5 +57,27 @@ public class TilableEditableItem : SingleEditableItem
             allTiles = transform.GetComponentsInChildren<SpriteRenderer>();
             originalSprite = allTiles[0].sprite;
         }
+    }
+
+    IEnumerator ShowEffect()
+    {
+        float timer = 0;
+        while (timer < 1)
+        {
+            floorMaterial.SetFloat("_Intensity",timer / 3);
+            timer += Time.deltaTime * 4;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        while (timer > 0)
+        {
+            floorMaterial.SetFloat("_Intensity", timer / 3);
+            timer -= Time.deltaTime * 4;
+            yield return null;
+        }
+
+        floorMaterial.SetFloat("_Intensity", 0);
     }
 }
