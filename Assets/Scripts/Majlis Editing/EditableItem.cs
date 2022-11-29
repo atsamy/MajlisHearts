f53cce0dcc4b116c72dcfa1663ea0b918808b04b
atsamy <amtsamy@gmail.com>
2022-11-29 19:35:52 +0700

    khod ya ghamry

--- Assets/Scripts/Majlis Editing/EditableItem.cs
+++ Assets/Scripts/Majlis Editing/EditableItem.cs
@@ -174,16 +174,16 @@ public class EditableItem : MonoBehaviour, IJsonTask
 
     public void ShowGlowEffect()
     {
-        effectMaterial.SetFloat("_Intensity", 0.4f);
+        effectMaterial.SetFloat("_Intensity", 0.35f);
 
-        effectMaterial.DOFloat(1f, "_Progress", 1f).SetEase(Ease.Flash).OnComplete(() =>
+        effectMaterial.DOFloat(1f, "_Progress", 0.75f).SetEase(Ease.Flash).OnComplete(() =>
         {
             effectMaterial.SetFloat("_Progress", 0f);
-            effectMaterial.SetFloat("_Intensity", 0.2f);
+            effectMaterial.SetFloat("_Intensity", 0.25f);
 
-            effectMaterial.DOFloat(0, "_Intensity", 0.75f).SetEase(Ease.Flash);
+            effectMaterial.DOFloat(0, "_Intensity", 0.5f).SetEase(Ease.Flash);
 
-            effectMaterial.DOFloat(1f, "_Progress", 1f).SetEase(Ease.Flash).OnComplete(() => effectMaterial.SetFloat("_Progress", 0f));
+            effectMaterial.DOFloat(1f, "_Progress", 0.75f).SetEase(Ease.Flash).OnComplete(() => effectMaterial.SetFloat("_Progress", 0f));
         });
     }
 
