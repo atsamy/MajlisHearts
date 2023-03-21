33ee2ddaa55f078d7beb194083a429e121ed774b
Ahmed <amtsamy@gmail.com>
2023-03-21 17:06:20 +0200

    adjustments

---
+++ Assets/Scripts/LeaderboardEntry.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardEntry : MonoBehaviour
+{
+    [SerializeField]
+    Image avatar;
+
+    [SerializeField]
+    TextMeshProUGUI rankText;
+
+    [SerializeField]
+    Image rankImage;
+
+    [SerializeField]
+    Sprite[] rankSprites;
+
+    [SerializeField]
+    Image cup;
+
+    [SerializeField]
+    Sprite[] cupSprites;
+
+    [SerializeField]
+    Image frame;
+
+    [SerializeField]
+    Image[] innerFrames;
+
+    [SerializeField]
+    Sprite frameSprite;
+
+    [SerializeField]
+    Sprite innerFrameSprite;
+
+    [SerializeField]
+    TextMeshProUGUI nameText;
+
+    [SerializeField]
+    TextMeshProUGUI pointsText;
+
+    public void Set(int rank,string name,int points,string avatarName)
+    {
+        nameText.text = name;
+        pointsText.text = points.ToString();
+
+        if (rank < 4)
+        {
+            cup.sprite = cupSprites[rank - 1];
+            cup.gameObject.SetActive(true);
+            rankImage.gameObject.SetActive(true);
+            rankImage.sprite = rankSprites[rank - 1];
+
+            rankText.gameObject.SetActive(false);
+        }
+        else 
+        {
+            cup.gameObject.SetActive(false);
+            rankText.gameObject.SetActive(true);
+            rankText.text = rank.ToString();
+        }
+
+        if (name == GameManager.Instance.MyPlayer.Name)
+        {
+            frame.sprite = frameSprite;
+
+            foreach (var item in innerFrames)
+            {
+                item.sprite = innerFrameSprite;
+            }
+
+            avatar.sprite = AvatarManager.Instance.playerAvatar;
+        }
+        else
+        {
+            print(avatarName);
+            avatar.sprite = AvatarManager.Instance.GetAvatarSprite(avatarName);
+        }
+    }
+}
---
+++ Assets/Scripts/LeaderboardPanel.cs
@@ -0,0 +1,62 @@
+//using PlayFab.ClientModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardPanel : MenuScene
+{
+    [SerializeField]
+    GameObject leaderBoardEntry;
+
+    [SerializeField]
+    Button[] tabsButtons;
+
+    [SerializeField]
+    Transform[] contents;
+
+    private void Awake()
+    {
+        PlayfabManager.instance.GetHeartsLeaderboard((entries) => 
+        {
+            foreach (var entry in entries) 
+            {
+                LeaderboardEntry newEntry = Instantiate(leaderBoardEntry, contents[0]).GetComponent<LeaderboardEntry>();
+                newEntry.Set(entry.Position + 1,entry.Profile.DisplayName,entry.StatValue,entry.Profile.AvatarUrl);
+            }
+        });
+
+        PlayfabManager.instance.GetHeartsFriendsLeaderboard((entries) =>
+        {
+            foreach (var entry in entries)
+            {
+                LeaderboardEntry newEntry = Instantiate(leaderBoardEntry, contents[2]).GetComponent<LeaderboardEntry>();
+                newEntry.Set(entry.Position + 1, entry.DisplayName, entry.StatValue, entry.Profile.AvatarUrl);
+            }
+        });
+
+        PlayfabManager.instance.GetHeartsLeaderboardAroundPlayer((entries) =>
+        {
+            foreach (var entry in entries)
+            {
+                LeaderboardEntry newEntry = Instantiate(leaderBoardEntry, contents[1]).GetComponent<LeaderboardEntry>();
+                newEntry.Set(entry.Position + 1, entry.DisplayName, entry.StatValue, entry.Profile.AvatarUrl);
+            }
+        });
+    }
+
+    public void SwitchTab(int index)
+    {
+        for (int i = 0; i < tabsButtons.Length; i++)
+        {
+            tabsButtons[i].interactable = (i != index);
+            contents[i].gameObject.SetActive(i == index);
+        }
+    }
+
+    public override void Close()
+    {
+        MenuManager.Instance.ShowMain();
+        base.Close();
+    }
+}
